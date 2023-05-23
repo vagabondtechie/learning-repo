c393ac8c88c2737e4325f760bcf5513d45947493
vagabondtechie <dev@maharana.me>
2023-05-23 11:07:32 +0530

    How to use variables in bash script

---
+++ bash-scripting/variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Declaring variables
+FIRST_NAME=Abhishek
+LAST_NAME=Maharana
+SALARY=100
+
+# Using variables
+echo Hi. My name is $FIRST_NAME $LASTNAME
+echo I earn an yearly income of USD $(($SALARY * 12))
+
